46ff5dfc7862d8bdc34616abf11c1376159471b5
basedgrit <dw.is.life11@gmail.com>
2026-02-13 13:09:30 -0500

    Add end turn functionality with button in Deck Builder

--- deckbuilder.py
+++ deckbuilder.py
@@ -6,6 +6,14 @@ root = tk.Tk()
 root.title("Deck Builder")
 root.geometry("900x600")
 
+### Function to end turn
+def end_turn():
+    log.insert("end", "Turn Ended\n")
+    log.see("end")
+
+
+
+
 
 ### Status Bar
 status = tk.Label(root, text="HP:50 | Block:0 | Energy:0 | Enemy HP: 30")
@@ -23,5 +31,10 @@ hand_frame.pack(fill="x")
 
 hand_label = tk.Label(hand_frame, text="Hand will go here")
 hand_label.pack(pady=10)
+
+### End Button
+end_button = tk.Button(hand_frame, text="End Turn", command=end_turn)
+end_button.pack(side="left",padx=10,pady=10)
+
 
 root.mainloop()
