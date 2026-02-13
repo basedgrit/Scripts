0f4868e9c4dfd10dd78cdfbf3cfaf6d7f393fed6
basedgrit <dw.is.life11@gmail.com>
2026-02-13 13:01:35 -0500

    Add status label to display game information in Deck Builder

--- deckbuilder.py
+++ deckbuilder.py
@@ -6,4 +6,8 @@ root = tk.Tk()
 root.title("Deck Builder")
 root.geometry("900x600")
 
+status = tk.Label(root, text="HP:50 | Block:0 | Energy:0 | Enemy HP: 30")
+
+status.pack(fill="x")
+
 root.mainloop()
